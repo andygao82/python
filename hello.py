188788e216337067d361c0ceb830e4b06636cab0
andygao82 <freedomandy82@gmail.com>
2019-11-04 21:57:11 +1100

    first commit python

---
+++ hello.py
@@ -0,0 +1,18 @@
+def hello():
+    return 'Hello world!'
+
+print(hello())
+
+string = "C:/node/npm"
+
+string1 = "kuma"
+string2 = "maku"
+
+print(string1 + string2)
+print(string1, string2)
+print(string)
+
+print(string[0])
+
+print(string[1:5])
+
